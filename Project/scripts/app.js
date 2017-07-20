bdb03ea2fa566c3ffba02bca0229b59fb1275e81
Timothy Ludwig <tludwig01@gmail.com>
2017-07-20 15:11:51 -0400

    Flustered AF

--- Project/scripts/app.js
+++ Project/scripts/app.js
@@ -26,52 +26,61 @@ $(() => {
 
 //upon clicking to begin the first div enters the DOM for player one.
 
+// First Round
+const roundOne = [
+  {"question" : "What's the name of the sword Bilbo Baggins gets from cave trolls? It glows blue when orcs are close. ", "correctAnswer" : "Sting", "incorrectAnswerOne" : "Glamdring", "incorrectAnswerTwo" : "Needle"},
+
+  {"question" : "When Gandalf shouts 'YOU SHALL NOT PASS!', what is the name of the creature he is shouting at?", "correctAnswer" : "Balrog", "incorrectAnswerOne" : "Nazgul", "incorrectAnswerTwo" : "Cave Troll"},
+
+  {"question" : "Orcs weren't always evil, nor in their current form. What were they once?", "correctAnswer" : "Elves", "incorrectAnswerOne" : "Men", "incorrectAnswerTwo" : "Dwarves"},
+
+  {"question" : "You know the bow wielding elf, Legolas. What's his father's name?", "correctAnswer" : "Thranduil", "incorrectAnswerOne" : "Elrond", "incorrectAnswerTwo" : "Celeborn"},
+
+  {"question" : "What was the name of the Skinchanger in 'The Hobbit' who turned into a bear?", "correctAnswer" : "Beorn", "incorrectAnswerOne" : "Radagast", "incorrectAnswerTwo" : "Bolg"},
+
+  {"question" : "How many great wizards are there?", "correctAnswer" : "Five", "incorrectAnswerOne" : "Three", "incorrectAnswerTwo" : "One"},
+
+  {"question" : "What was Gollum's name before he became Gollum?", "correctAnswer" : "Smeagol", "incorrectAnswerOne" : "Deagol", "incorrectAnswerTwo" : "Old Took"},
+
+  {"question" : "Name the actual 'Two Towers' the second book is named for.", "correctAnswer" : "Minas Tirith & Minas Ithil", "incorrectAnswerOne" : "Barad Dur & Isengard", "incorrectAnswerTwo" : "Helm's Deep & Osgiliath"},
+
+  {"question" : "What's Boromir's father's name?", "correctAnswer" : "Denethor", "incorrectAnswerOne" : "Imrahil", "incorrectAnswerTwo" : "Theoden"}
+];
+
 const playerOne = {
   name: "",
   score: 0,
-  getName(){
-    console.log("what's your name?");
+  getPoint(){
+    playerOne.score++;
   }
-}
+
+};
 
 const playerTwo = {
   name: "",
   score: 0,
-  getName(){
-    console.log("what's your name?");
+  getPoint(){
+    playerTwo.score++;
   }
-}
+};
+//PLAYERS//
 
-//Lord of the Rings
-const lordOfTheRingsTrivia = [
-  {"question" : "What's the name of the sword Bilbo Baggins gets from cave trolls? It glows blue when orcs are close. ", "correctAnswer" : "Sting", "incorrectAnswerOne" : "Glamdring", "incorrectAnswerTwo" : "Needle"},
-//player 1
+const gameState = {
+  questionNumber: 0,
+  player: playerOne,
+  round: roundOne,
+  changePlayer(){
+    if (gameState.player === playerOne){
+      gameState.player = playerTwo;
+    } else {
+      gameState.player = playerOne;
+    }
+  },
+  changeRound(){
 
-  {"question" : "When Gandalf shouts 'YOU SHALL NOT PASS!', what is the name of the creature he is shouting at?", "correctAnswer" : "Balrog", "incorrectAnswerOne" : "Nazgul", "incorrectAnswerTwo" : "Cave Troll"},
-//player 2
-
-  {"question" : "Orcs weren't always evil, nor in their current form. What were they once?", "correctAnswer" : "Elves", "incorrectAnswerOne" : "Men", "incorrectAnswerTwo" : "Dwarves"},
-//player 1
-
-  {"question" : "You know the bow wielding elf, Legolas. What's his father's name?", "correctAnswer" : "Thranduil", "incorrectAnswerOne" : "Elrond", "incorrectAnswerTwo" : "Celeborn"},
-//player 2
-
-  {"question" : "What was the name of the Skinchanger in 'The Hobbit' who turned into a bear?", "correctAnswer" : "Beorn", "incorrectAnswerOne" : "Radagast", "incorrectAnswerTwo" : "Bolg"},
-//player 1
-
-  {"question" : "How many great wizards are there?", "correctAnswer" : "Five", "incorrectAnswerOne" : "Three", "incorrectAnswerTwo" : "One"},
-//player 2
-
-  {"question" : "What was Gollum's name before he became Gollum?", "correctAnswer" : "Smeagol", "incorrectAnswerOne" : "Deagol", "incorrectAnswerTwo" : "Old Took"},
-//player 1
-
-  {"question" : "Name the actual 'Two Towers' the second book is named for.", "correctAnswer" : "Minas Tirith & Minas Ithil", "incorrectAnswerOne" : "Barad Dur & Isengard", "incorrectAnswerTwo" : "Helm's Deep & Osgiliath"},
-//player 2
-
-  {"question" : "What's Boromir's father's name?", "correctAnswer" : "Denethor", "incorrectAnswerOne" : "Imrahil", "incorrectAnswerTwo" : "Theoden"}
-// not entirely sure how to implement this, but the idea is there
-];
-
+  }
+};
+//GAME//
 
 let questionNumber = 0;
 
@@ -79,48 +88,56 @@ const setupQuestions = () => {
 
   const $question =
 
-  $('<div/>').addClass('clear').text(lordOfTheRingsTrivia[questionNumber].question);
+  $('<div/>').addClass('clear').text(gameState.round[gameState.questionNumber].question);
 
     $('body').append($question);
 
     const $answerA =
 
-    $('<button>').addClass('button').text(lordOfTheRingsTrivia[questionNumber].incorrectAnswerOne);
+    $('<button>').addClass('button').text(gameState.round[gameState.questionNumber].incorrectAnswerOne);
 
       $('body').append($answerA);
 
     const $answerB =
 
-    $('<button>').addClass('button').text(lordOfTheRingsTrivia[questionNumber].incorrectAnswerTwo);
+    $('<button>').addClass('button').text(gameState.round[gameState.questionNumber].incorrectAnswerTwo);
 
       $('body').append($answerB);
 
     const $answerC =
 
-    $('<button>').addClass('button').text(lordOfTheRingsTrivia[questionNumber].correctAnswer);
+    $('<button>').addClass('button').text(gameState.round[gameState.questionNumber].correctAnswer);
 
       $('body').append($answerC);
 
-        questionNumber++;
+        gameState.questionNumber++;
 
           $answerA.on('click', wrongAnswer);
           $answerB.on('click', wrongAnswer);
           $answerC.on('click', rightAnswer);
 };
 
-//Working, but the answer button spits out the correct answer on the same button every single time. How should I go about scrambling or randomizing them?
+//Ok, I'm stuck here.
+
+const roundTwo = () => {
+   console.log("works");
+ }
+
 
 const rightAnswer = () => {
 clearBoard();
-console.log('correct!');
+playerOne.getPoint();
+playerTwo.getPoint();
+console.log(playerOne.score);
+console.log(playerTwo.score);
 }
-// Add player points when right answer selected
-//Must affix scoreboard and perhaps message on the DOM.
 
 const wrongAnswer = () => {
 clearBoard();
   console.log("NOPE!");
 }
+
+
 // No points when wrong answer selected
 //must affix scoreboard and perhaps a message on the DOM.
 
@@ -135,8 +152,6 @@ const startGame = () => {
   $('#welcome').remove();
   $('#start').remove();
     setupQuestions();
-    playerOne.getName();
-    playerTwo.getName();
 }
 //Clears initial screen, starts trivia.
 
